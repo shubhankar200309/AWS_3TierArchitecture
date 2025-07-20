fbe93450becc71ebb60e1e721724378c6bb965b6
Shubhankar Gupta <shubhankargupta200309@gmail.com>
2025-07-20 17:20:58 +0530

    Add files via upload

---
+++ 3Tier Code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.c7e2ymaw09of.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '12345678',
+    DB_DATABASE : 'webappdb'
+});
